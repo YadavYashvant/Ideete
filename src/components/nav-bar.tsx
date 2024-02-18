1d5a33dd4dddec4c5a60aab49fbb2c7749b15f11
yadavyashvant <yashvantyadav855@gmail.com>
2024-02-18 21:43:37 +0530

    Setting up nextjs routing for signup and sign in

--- src/components/nav-bar.tsx
+++ src/components/nav-bar.tsx
@@ -56,10 +56,10 @@ export function NavBar() {
       </NavbarContent>
       <NavbarContent justify="end">
         <NavbarItem className="hidden lg:flex">
-          <Link href="#">Login</Link>
+          <Link href="/Auth">Login</Link>
         </NavbarItem>
         <NavbarItem>
-          <Button as={Link} color="primary" href="#" variant="flat">
+          <Button as={Link} color="primary" href="/Auth" variant="flat">
             Sign Up
           </Button>
         </NavbarItem>
